d23110823cedb286153fc068c497bfd14b118f7a
Victor <victoorres@icloud.com>
2026-05-20 13:44:16 -0300

    fix: corrigindo layout global css

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { Analytics } from "@vercel/analytics/next";
 import { Toaster as SonnerToaster } from "sonner";
+import "./globals.css";
 import { Toaster } from "@/components/ui/toaster";
 import Script from "next/script";
 
